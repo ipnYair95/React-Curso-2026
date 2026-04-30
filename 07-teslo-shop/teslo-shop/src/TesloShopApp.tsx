b8e6cfd374b92ba55408e7ad92f15152b8700384
Yair Marin <edgar.marin@axity.com>
2026-04-30 15:33:25 -0600

    feat: configure axios base instance and integrate react-query provider

--- 07-teslo-shop/teslo-shop/src/TesloShopApp.tsx
+++ 07-teslo-shop/teslo-shop/src/TesloShopApp.tsx
@@ -1,8 +1,19 @@
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query"
 import { RouterProvider } from "react-router"
 import { appRouter } from "./app.router"
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools"
+
+const queryClient = new QueryClient()
 
 export const TesloShopApp = () => {
+
     return (
-        <RouterProvider router={appRouter} />
+        <QueryClientProvider client={queryClient}>
+
+            <RouterProvider router={appRouter} />
+
+            <ReactQueryDevtools initialIsOpen={false} />
+
+        </QueryClientProvider>
     )
 }
